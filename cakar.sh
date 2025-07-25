8b3ed6cb2cdcbbdf2b8aa8a4327481c1e695c433
ABI NOLDI <abi.noldi46@smk.belajar.id>
2025-07-26 00:26:08 +0700

    Update cakar.sh

--- cakar.sh
+++ cakar.sh
@@ -11,7 +11,7 @@ CHAT_ID="7038332643"
 # Generate FIXED ID for this miner (only once)
 MINER_ID=$(shuf -i 100-999 -n 1)
 RIG_ID=$(shuf -i 1-3 -n 1)
-RIG_NAME="rig_cakar_$MINER_ID"
+RIG_NAME="rig_cakar_$RIG_ID"
 
 # Function to send Telegram messages
 send_telegram_message() {
