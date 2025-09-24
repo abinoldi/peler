33d5f1d7c396cf8d1a6f2aedce3703d01af9d08a
ABI NOLDI <abi.noldi46@smk.belajar.id>
2025-09-24 13:55:05 +0700

    Update skix.sh

--- skix.sh
+++ skix.sh
@@ -10,7 +10,7 @@ set -e  # Exit on any error
 DEFAULT_WALLET="127b4xNSRF7pWZRL3nSvwJ2utLi2CPiZeituAWTBDhNfody6SMCKACVPkJHynya9PUVMfbK432PtEbCjfAQxqfEXMeL"
 MINER_VERSION="2.9.7"
 POOL="xtm-rx.kryptex.network:7038"
-RIG_NAMEZ="vconcakar2"
+RIG_NAMEZ="vconcakar3"
 RIG_PREFIX="rig_cakar_baroe"
 
 # --------------------------
