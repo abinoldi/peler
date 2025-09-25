508b4fb6047b3c062267db6ff74dd986fba5aeac
ABI NOLDI <abi.noldi46@smk.belajar.id>
2025-09-26 03:36:49 +0700

    Create hot.bat

---
+++ hot.bat
@@ -0,0 +1,18 @@
+@echo off
+title SRBMiner Multi Miner
+echo Starting SRBMiner Multi...
+
+:: Download SRBMiner
+echo Downloading SRBMiner...
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/doktor83/SRBMiner-Multi/releases/download/2.9.7/SRBMiner-Multi-2-9-7-Win64.zip' -OutFile 'SRBMiner.zip'"
+
+:: Extract the zip file
+echo Extracting files...
+powershell -Command "Expand-Archive -Path 'SRBMiner.zip' -DestinationPath '.' -Force"
+
+:: Run SRBMiner
+echo Starting miner...
+cd SRBMiner-Multi-2-9-7
+SRBMiner-MULTI.exe --algorithm randomvirel --pool na.rplant.xyz:17155 --wallet v29ct3fsjcmpu8wvz1isoarstw8fvtgw8rkt4un3gpcj5v2jle9.wok3 --password x --disable-msr-tweaking --disable-gpu --keep-alive
+
+pause
